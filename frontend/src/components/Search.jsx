66cdf5d6a69fd432388d49025389442b9df2f495
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-08 14:37:15 -0500

    implemented basic search and filter

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 function Search() {
   // For testing - to be deleted
+  const songs = [
+    { id: 1, songTitle: "Country Roads", artistTitle: "John Denver" },
+    { id: 2, songTitle: "Sweet Caroline", artistTitle: "Neil Diamond" },
+    { id: 3, songTitle: "Piano Man", artistTitle: "Billy Joel" },
+  ];
   const [searchItem, setSearchItem] = useState("");
+  const [filteredSongs, setFilteredSongs] = useState(songs);
 
   const handleInput = (e) => {
     const searchTerm = e.target.value;
     console.log(searchTerm);
     setSearchItem(searchTerm);
+
+    const filteredItems = songs.filter((song) =>
+      song.songTitle.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredSongs(filteredItems);
   };
   return (
     <div>
@@ -17,6 +29,11 @@ function Search() {
         onChange={handleInput}
         placeholder="Search for a song"
       />
+      <ul>
+        {filteredSongs.map((song) => (
+          <li key={song.id}>{song.songTitle}</li>
+        ))}
+      </ul>
     </div>
   );
 }
