51cd77200cb07540c938d796f5e1abbd0c21fd97
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-08 15:27:03 -0500

    working on songController

---
+++ backend/controllers/songController.js
@@ -0,0 +1,11 @@
+// needs installation
+const asyncHandler = require("express-async-handler");
+
+const Song = require("../models/songModel");
+
+// @desc    Search for a song
+// @route   GET /api/songs
+// @access  Public
+const getSong = asyncHandler(async (req, res) => {
+  const song = await Song.findById(req.song.id);
+});
