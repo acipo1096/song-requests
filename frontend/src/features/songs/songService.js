475e645119317f92d56fa23b48802be4d655b39f
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-13 13:20:40 -0500

    how do I connect backend to redux?

--- frontend/src/features/songs/songService.js
+++ frontend/src/features/songs/songService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/songs/";
+const API_URL = "/";
 
 // Get songs
 const getSongs = async () => {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { get } from "mongoose";
+import { getSongs } from "../features/songs/songSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function Home() {
+  const dispatch = useDispatch();
+
+  dispatch(getSongs);
   return (
     <div>
       <h1>Home</h1>
