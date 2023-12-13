22982314fa234d788ad893f0fcf6916aa30bcb39
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-13 14:50:42 -0500

    moved code from Home to Search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { get } from "mongoose";
+import { useState, useEffect } from "react";
+import { getSongs, reset } from "../features/songs/songSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function Search() {
+  const { songs, isLoading, isSuccess } = useSelector((state) => state.songs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSongs());
+    console.log(getSongs);
+  }, []);
+
   // For testing - to be deleted
-  const songs = [
-    { id: 1, songTitle: "Country Roads", artistTitle: "John Denver" },
-    { id: 2, songTitle: "Sweet Caroline", artistTitle: "Neil Diamond" },
-    { id: 3, songTitle: "Piano Man", artistTitle: "Billy Joel" },
-  ];
+  // const songs = [
+  //   { id: 1, songTitle: "Country Roads", artistTitle: "John Denver" },
+  //   { id: 2, songTitle: "Sweet Caroline", artistTitle: "Neil Diamond" },
+  //   { id: 3, songTitle: "Piano Man", artistTitle: "Billy Joel" },
+  // ];
   const [searchItem, setSearchItem] = useState("");
   const [filteredSongs, setFilteredSongs] = useState(songs);
 
@@ -35,6 +46,11 @@ function Search() {
           <li key={song.id}>{song.songTitle}</li>
         ))}
       </ul>
+      {songs.map((song) => (
+        <div key={song._id} song={songs}>
+          {song.artist} - {song.song}
+        </div>
+      ))}
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,22 +4,9 @@ import { getSongs, reset } from "../features/songs/songSlice";
 import { useSelector, useDispatch } from "react-redux";
 
 function Home() {
-  const { songs, isLoading, isSuccess } = useSelector((state) => state.songs);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getSongs());
-    console.log(getSongs);
-  }, []);
-
   return (
     <div>
       <h1>Home</h1>
-      {songs.map((song) => (
-        <div key={song._id} song={songs}>
-          {song.artist} - {song.song}
-        </div>
-      ))}
     </div>
   );
 }
