ece66451a1699588f60c3e35c27f2234fc8e6093
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-14 11:15:46 -0500

    imported react-email and resend and created first email template

---
+++ frontend/src/components/Email.jsx
@@ -0,0 +1,14 @@
+import { Html } from "@react-email/html";
+import { Button } from "@react-email/button";
+
+export function Email(props) {
+  const { url } = props;
+}
+
+return (
+  <Html lang="en">
+    <div>This is a test email! Hello!</div>
+  </Html>
+);
+
+export default Email;
