4483bf06d2ffab3b6661f2256cebdd322e3d04fd
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-15 13:32:10 -0500

    created footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        <Footer />
       </Router>
     </>
   );
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { FaRegCopyright } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer>
+      <FaRegCopyright /> 2023 Alex Floyd
+    </footer>
+  );
+}
+
+export default Footer;
