6c386fe975e8505e18cd1ddaf6dca322742248f2
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-18 11:06:52 -0500

    began work on Thanks page

--- frontend/src/pages/Thanks.jsx
+++ frontend/src/pages/Thanks.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useState, useReact } from "react";
+import { FaVenmo } from "react-icons/fa";
 
 function Thanks() {
-  return <div>Thank you!</div>;
+  return (
+    <div>
+      <p>Thanks for making your request!</p>
+      <p>If I don't play your song within a few songs from now, let me know</p>
+      <p>
+        If I take a break, I'll play your song as one of the first in my next
+        set
+      </p>
+      <p>If you like what you here, consider buying me a coffee:</p>
+      <div>
+        <FaVenmo />
+      </div>
+    </div>
+  );
 }
 
 export default Thanks;
