ef843e44121b613f75647fc385082ab5bf8438b5
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-19 08:23:33 -0500

    removed unnecessary dependencies from Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,3 @@
-import { get } from "mongoose";
-import { useState, useEffect } from "react";
-import { getSongs, reset } from "../features/songs/songSlice";
-import { useSelector, useDispatch } from "react-redux";
 import Search from "../components/Search";
 
 function Home() {
