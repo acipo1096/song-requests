7d8e39d3866617bbe6b60a160281b87205c82ed0
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-19 09:08:46 -0500

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
