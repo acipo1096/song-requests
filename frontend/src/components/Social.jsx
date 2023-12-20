237c141a6b1ec64751d3847c438d45d8e2edebd2
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-20 08:37:24 -0500

    changed social css

--- frontend/src/components/Social.jsx
+++ frontend/src/components/Social.jsx
@@ -7,22 +7,27 @@ function Social() {
   return (
     <div>
       <div className="social-media">
-        <Link
-          className="venmo"
-          to="https://venmo.com/u/alexcip96"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IoLogoVenmo />
-        </Link>
-        <Link
-          className="instagram"
-          to="https://www.instagram.com/alexfloyd_music/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagramSquare /> Follow me on Instagram
-        </Link>
+        <div>
+          <Link
+            className="venmo"
+            to="https://venmo.com/u/alexcip96"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoVenmo />
+          </Link>
+        </div>
+        <p className="insta-text">Follow me on Instagram</p>
+        <div>
+          <Link
+            className="instagram"
+            to="https://www.instagram.com/alexfloyd_music/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagramSquare />
+          </Link>
+        </div>
       </div>
       <div className="back-link">
         <Link className="back-link-text" to="/">
