cc710592c050369611016f8d529211df57c5de48
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-21 09:39:53 -0500

    changed to toast warning

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -42,8 +42,7 @@ function Search() {
 
   useEffect(() => {
     if (isLoading) {
-      toast.info(message);
-      console.log("HUH");
+      toast.warning(message);
     }
 
     dispatch(getSongs());
