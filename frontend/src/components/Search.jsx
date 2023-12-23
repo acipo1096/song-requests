3d811ce1e6e16f4411a1e5a8665ad60a49984d1d
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-23 01:35:05 -0500

    no spinner

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -7,7 +7,6 @@ import { useState, useEffect } from "react";
 import { getSongs, reset } from "../features/songs/songSlice";
 import { useSelector, useDispatch } from "react-redux";
 import Suggest from "./Suggest";
-import Spinner from "./Spinner";
 // import TotalSongs from "./TotalSongs";
 
 const customStyles = {
