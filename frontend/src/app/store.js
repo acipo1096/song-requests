f7d3522ac32df89c5a8ff7e8f0a0eaf26d55ad70
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-31 18:46:03 -0500

    try second fix

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -11,6 +11,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, songReducer);
 
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
+
 export const store = configureStore({
   reducer: { songs: persistedReducer },
 });
