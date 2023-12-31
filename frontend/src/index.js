b6ceb3c8549054e75968e76c9419d59deeb1b14c
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-31 18:38:24 -0500

    try fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -8,6 +9,7 @@ import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
