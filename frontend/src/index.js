a590012344c9ccd2c27cecba0520ddc9d268bf1d
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-31 19:08:06 -0500

    installed redux-persist on frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
