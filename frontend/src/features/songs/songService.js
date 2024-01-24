49273d8487516f81866385325a7415807d347363
Alex Cipollone <alex.cipollone10@gmail.com>
2024-01-24 09:38:48 -0500

    tried optimizing songService by implementing caching

--- frontend/src/features/songs/songService.js
+++ frontend/src/features/songs/songService.js
@@ -2,11 +2,22 @@ import axios from "axios";
 
 const API_URL = "https://song-requests-backend.onrender.com/api/songs/";
 
+// From ChatGPT - introduce caching to avoid unnecessary calls
+let cachedSongs = null;
+
 // Get songs
 const getSongs = async () => {
-  const response = await axios.get(API_URL);
+  try {
+    if (cachedSongs) return cachedSongs;
 
-  return response.data;
+    const response = await axios.get(API_URL);
+
+    cachedSongs = response.data;
+    return cachedSongs;
+  } catch (error) {
+    console.error("Error fetching songs!", error);
+    throw error;
+  }
 };
 
 const songService = {
