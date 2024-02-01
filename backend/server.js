b06dff877db9a6dbd338c09ce78807be4105e887
Alex Cipollone <alex.cipollone10@gmail.com>
2024-01-31 23:33:10 -0500

    removed "deploying to heroku" code

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,4 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/songs", require("./routes/songRoutes"));
 
-// DEPLOYING TO HEROKU
-if (process.env.NODE_ENV === "production") {
-  // Set static path/build folder
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  // * means app.anything else other than our routes defined above
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  );
-
-  // In package.json
-  // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend",
-}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
