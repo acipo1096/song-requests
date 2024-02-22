1c535839769b192cf4da2cd6aaae6adcd5a1a1cc
Alex Cipollone <alex.cipollone10@gmail.com>
2024-02-21 19:00:48 -0500

    list correct

--- frontend/src/features/songs/songService.js
+++ frontend/src/features/songs/songService.js
@@ -16,7 +16,6 @@ const getSongs = async () => {
     // return cachedSongs;
 
     const response = await axios.get(API_URL);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching songs!", error);
